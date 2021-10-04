4939acb0fd47efb63c50e965099d7dec17f041c0
emon2058 <ashad2058@gmail.com>
2021-10-04 23:38:52 +0600

    home done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import Header from './components/Header/Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Footer from './components/Footer/Footer';
+import Home from './components/Home/Home';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Home></Home>
+      <Footer></Footer>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,30 @@
 import React from 'react';
-import { Container, Nav, Navbar } from 'react-bootstrap';
-
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import './Header.css';
 const Header = () => {
     return (
         <div>
-            <Navbar bg="light" variant="light">
-                <Container>
-                <Navbar.Brand href="#home">Home</Navbar.Brand>
-                <Nav className="me-auto">
-                    <Nav.Link href="#home">Services</Nav.Link>
-                    <Nav.Link href="#features">About</Nav.Link>
-                    <Nav.Link href="#pricing">Login</Nav.Link>
-                </Nav>
-                </Container>
+            <Navbar bg="light" expand="lg">
+                <Navbar.Brand href="home">Home</Navbar.Brand>
+                <Navbar.Toggle aria-controls="navbarScroll" />
+                <Navbar.Collapse id="navbarScroll">
+                    <Nav
+                    className="mr-auto my-2 my-lg-0"
+                    style={{ maxHeight: '100px' }}
+                    navbarScroll
+                    >
+                        <Nav.Link href="services">Services</Nav.Link>
+                        <Nav.Link href="about">About</Nav.Link>
+                        <Nav.Link href="signup">Sign up</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
             </Navbar>
+            <div className="header">
+                <h1>ONLINE LEARNING</h1>
+                <h1>Learn Everyday</h1>
+                <p>Learning something new every day is one of the top ways to keep the mind sharp. The brain is like a muscle and needs to be exercised</p>
+            </div>
         </div>
     );
 };
