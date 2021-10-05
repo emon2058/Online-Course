3328eae58b70e4157d2de5694f16c42b1169b898
emon2058 <ashad2058@gmail.com>
2021-10-05 17:01:24 +0600

    contact component done

--- src/components/Courses/Courses.js
+++ src/components/Courses/Courses.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const About = () => {
+const Courses = () => {
     return (
         <div>
-            <h1>About</h1>
+            <h1>Course</h1>
         </div>
     );
 };
 
-export default About;
+export default Courses;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,25 +1,29 @@
 import React from 'react';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { NavLink } from 'react-router-dom';
 import './Header.css';
 const Header = () => {
     return (
         <div>
-            <Navbar bg="light" expand="lg">
-                <Navbar.Brand href="home">Home</Navbar.Brand>
-                <Navbar.Toggle aria-controls="navbarScroll" />
-                <Navbar.Collapse id="navbarScroll">
-                    <Nav
-                    className="mr-auto my-2 my-lg-0"
-                    style={{ maxHeight: '100px' }}
-                    navbarScroll
-                    >
-                        <Nav.Link href="services">Services</Nav.Link>
-                        <Nav.Link href="about">About</Nav.Link>
-                        <Nav.Link href="signup">Sign up</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Navbar>
+            <nav>                    
+                <Navbar bg="light" expand="lg">
+                    <NavLink to="/">Online Course</NavLink>
+                    <Navbar.Toggle aria-controls="navbarScroll" />
+                    <Navbar.Collapse id="navbarScroll">
+                        <Nav
+                        className="mr-auto my-2 my-lg-0"
+                        style={{ maxHeight: '100px' }}
+                        navbarScroll
+                        >
+                            <NavLink to="/home">Home</NavLink>
+                            <NavLink to="/courses">Courses</NavLink>
+                            <NavLink to="/about">About</NavLink>
+                            <NavLink to="/contact">Contact</NavLink>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
+            </nav>
             <div className="header">
                 <h1>ONLINE LEARNING</h1>
                 <h1>Learn Everyday</h1>
